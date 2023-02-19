901d2234e510da6473d2d5b82b7f287934cc19ef
kelvinsyano <wbilly29@yahoo.com>
2023-02-19 14:32:30 -0800

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ *
+ * Return: it will return 1
+ */
+int main(void)
+{
+	stderr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
