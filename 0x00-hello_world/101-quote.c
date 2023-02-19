3efdc87824111420ee91232395071320bdfb3dd6
kelvinsyano <wbilly29@yahoo.com>
2023-02-19 14:54:20 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - this is main function
  *
- * Return: it will return 1
+ * Return: always 1
  */
 int main(void)
 {
-	stderr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
