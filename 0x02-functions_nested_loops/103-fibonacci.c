f36b553326456290d455c626789e2cf97d5516d6
kelvinsyano <wbilly29@yahoo.com>
2023-02-22 19:18:42 -0800

    fibo4k

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a = 1, b = 2, c, sum = 0;
+
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+		{
+			sum += b;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
