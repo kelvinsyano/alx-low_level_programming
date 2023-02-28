c1388b6352f84ccf469ff1718d8b4cbb85f410ea
kelvinsyano <wbilly29@yahoo.com>
2023-02-27 18:07:40 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse function
+ *
+ * @s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len > 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+	_putchar('\n');
+}
