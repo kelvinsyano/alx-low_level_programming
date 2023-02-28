edaecd13dea6c00c0670ced239efe23b5f9524e2
kelvinsyano <wbilly29@yahoo.com>
2023-02-27 18:22:44 -0800

    reverse function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - function to reverse string
+ *
+ * @s: parameter
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	char *start = s, *end = s, tmp;
+
+	while (*end)
+	{
+		end++;
+		len++;
+	}
+	while (start < end)
+	{
+		tmp = *start;
+		*start++ = *end;
+		*end-- = tmp;
+	}
+}
