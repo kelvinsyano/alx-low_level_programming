9ed12ab146d0f4749e0272bd531607366e8d0dd9
kelvinsyano <wbilly29@yahoo.com>
2023-02-27 18:44:03 -0800

    jhghj

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ *
+ * @str: parameter
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
