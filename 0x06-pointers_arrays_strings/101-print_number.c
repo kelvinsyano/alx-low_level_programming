b88c2a89509eb0c9721daa3f7c7f6457ea21e14d
kelvinsyano <wbilly29@yahoo.com>
2023-03-02 15:04:23 -0800

    numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer using _putchar
+ *
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
