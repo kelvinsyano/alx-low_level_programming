8beecf48057391eaefcf23c01f5cca948a317cb2
kelvinsyano <wbilly29@yahoo.com>
2023-03-07 15:38:33 -0800

    diagnol addition

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,8 +15,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 = a[i * size + i];
-		sum2 = a[(i + 1) * size - i - 1];
+		sum1 = sum1 + a[i * size + i];
+		sum2 = sum2 + a[(i + 1) * size - i - 1];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
