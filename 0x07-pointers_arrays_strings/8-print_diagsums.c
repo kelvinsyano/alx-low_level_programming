2df96a1c99898e9b4c4778079c3ab3a49a611280
kelvinsyano <wbilly29@yahoo.com>
2023-03-07 14:50:21 -0800

    diagnonal addition

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - printing sum of diagnols
+ *
+ * @a: parameter of an rray
+ *
+ * @size: size of an array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0, i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = a[i * size + i];
+		sum2 = a[(i + 1) * size - i - 1];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
