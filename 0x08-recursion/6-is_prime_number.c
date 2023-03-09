f03129ba2274a5a7887e10167347dce6bbefd061
kelvinsyano <wbilly29@yahoo.com>
2023-03-09 12:55:30 -0800

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime numbers
+ *
+ * @n: parametre
+ *
+ * Return: 1 if prime 0 not
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	for (i = 3; i < (n / 2); i += 2)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
