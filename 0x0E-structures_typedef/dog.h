11874f34c1bc8d56de570dbb3a078de31ad97d34
kelvinsyano <wbilly29@yahoo.com>
2023-03-20 17:53:52 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
+/**
+ * struct dog - my struct
+ * @name: first name
+ * @age: second name
+ * @owner: third name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
