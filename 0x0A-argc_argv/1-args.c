ff354029191b1b11431623ca81e28d141339a3e5
kelvinsyano <wbilly29@yahoo.com>
2023-03-28 08:36:40 -0700

    print a number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - min function
+ * @argc: first
+ * @argv: second
+ * Return: returns 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
