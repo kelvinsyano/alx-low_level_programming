e4a2604881c51b226ab23675d5382ae65d6d4c0e
kelvinsyano <wbilly29@yahoo.com>
2023-03-30 05:39:26 -0700

    prnt lists in linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all in linked list
+ * @h: node
+ * Return: returns total elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
